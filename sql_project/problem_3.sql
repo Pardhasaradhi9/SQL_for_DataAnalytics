a03dd7b337b3ffc8937952b0be2bdd4dd972a23a
Pardhasaradhi Chukka <pardhuchukka9927@gmail.com>
2024-10-14 22:32:41 +0530

    added plots

--- sql_project/problem_3.sql
+++ sql_project/problem_3.sql
@@ -8,11 +8,36 @@ Question: What are the most in-demand skills for Data Science?
 */
 
 SELECT
-    *
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
 FROM
     job_postings_fact
 INNER JOIN skills_job_dim ON
     job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON
     skills_job_dim.skill_id = skills_dim.skill_id
-LIMIT 10;    
+WHERE
+    job_title_short LIKE '%Data Scien%'
+GROUP BY
+    skills    
+ORDER BY
+    demand_count DESC 
+LIMIT 10;
+------------------------------------------------
+SELECT
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM
+    job_postings_fact
+INNER JOIN skills_job_dim ON
+    job_postings_fact.job_id = skills_job_dim.job_id
+INNER JOIN skills_dim ON
+    skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_title_short LIKE '%Data Scien%'
+    AND job_location LIKE '%India'
+GROUP BY
+    skills    
+ORDER BY
+    demand_count DESC 
+LIMIT 10;
